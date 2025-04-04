1156d11db6555fc98ed99e23d54a23b98c435456
Bhargav Annem <bannem@caltech.edu>
2025-04-04 12:29:00 -0700

    feat(wip): hash to g1

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,10 @@
+use substrate_bn::GroupError;
+
 pub mod g1;
 pub mod g2;
+pub trait HashToG1: Sized {
+    type FieldElement;
+    fn sgn0(x: Self::FieldElement) -> u64;
+    fn map_to_curve(u: Self::FieldElement) -> Result<Self, GroupError>;
+    fn hash(msg: &[u8], dst: &[u8]) -> Self;
+}
