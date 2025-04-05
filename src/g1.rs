bee62c1abab5906e4cb7f719e09bda15e4b9c89c
Bhargav Annem <bannem@caltech.edu>
2025-04-04 23:40:44 -0700

    feat(wip): tests

--- src/g1.rs
+++ src/g1.rs
@@ -1,3 +1,5 @@
+use std::mem::transmute;
+
 use digest::{consts::U32, generic_array::GenericArray};
 use num_bigint::BigUint;
 use substrate_bn::{AffineG1, Fq, GroupError};
@@ -74,7 +76,7 @@ fn hash_to_field(msg: &[u8], dst: &[u8], count: usize) -> Vec<Fq> {
         .map(|i| {
             let start = i * LEN_PER_ELM;
             let end = start + LEN_PER_ELM;
-            Fq::from_slice(&uniform_bytes[start..end])
+            Fq::from_be_bytes_mod_order(&uniform_bytes[start..end])
                 .expect("Invalid field element encoding")
         })
         .collect()
@@ -100,11 +102,17 @@ impl HashToG1 for AffineG1 {
     }
 
     fn map_to_curve(u: Fq) -> Result<Self, GroupError> {
-        let z: Fq = Fq::from_str("6350874878119819312338956282401532409788428879151445726012394534686998597021").unwrap();
-        let c1: Fq = Fq::from_str("3515256640640002027109419384348854550457404359307959241360540244102768179501").unwrap();
-        let c2: Fq = Fq::from_str("7768683996859727954953724731427871339453941139073188968338321679979113805781").unwrap();
-        let c3: Fq = Fq::from_str("5174556184976127869173189452163337195348491024958816448391141365979064675186").unwrap();
-        let c4: Fq = Fq::from_str("2609072103093089037936242735953952295622231240021995565748958972744717830193").unwrap();
+        let mut z: Fq = Fq::from_str("6350874878119819312338956282401532409788428879151445726012394534686998597021").unwrap();
+        let mut c1: Fq = Fq::from_str("3515256640640002027109419384348854550457404359307959241360540244102768179501").unwrap();
+        let mut c2: Fq = Fq::from_str("7768683996859727954953724731427871339453941139073188968338321679979113805781").unwrap();
+        let mut c3: Fq = Fq::from_str("5174556184976127869173189452163337195348491024958816448391141365979064675186").unwrap();
+        let mut c4: Fq = Fq::from_str("2609072103093089037936242735953952295622231240021995565748958972744717830193").unwrap();
+
+        z = z * Fq::one().inverse().unwrap();
+        c1 = c1 * Fq::one().inverse().unwrap();
+        c2 = c2 * Fq::one().inverse().unwrap();
+        c3 = c3 * Fq::one().inverse().unwrap();
+        c4 = c4 * Fq::one().inverse().unwrap();
 
         let mut tv1: Fq = u * u;       //    1.  tv1 = u²
         tv1 = tv1 * c1;                     //    2.  tv1 = tv1 * c1
@@ -133,26 +141,26 @@ impl HashToG1 for AffineG1 {
         x3 = x3 * tv3;                      //    23.  x3 = x3 * tv3
         x3 = x3 * x3;                   //    24.  x3 = x3²
         x3 = x3 * c4;                       //    25.  x3 = x3 * c4
-    
         x3 = x3 + z;                        //    26.  x3 = x3 + Z
-    
+
         let mut x = if gx1.sqrt().is_some() { x1 } else { x3};
 
+        // if gx2.legendre().is_qr() && !gx1.legendre().is_qr() {
         if gx2.sqrt().is_some() && !gx1.sqrt().is_some() { x = x2 } //    28.   x = CMOV(x, x2, e2)    # x = x2 if gx2 is square and gx1 is not
         
         let mut gx = x * x;    //    29.  gx = x²
         //    30.  gx = gx + A
         gx = gx * x;                //    31.  gx = gx * x
         gx = gx + Fq::from_str("3").unwrap();      //    32.  gx = gx + B
-    
         let mut y: Fq = gx.sqrt().unwrap();     //    33.   y = sqrt(gx)
-    
-    
-        let signs_not_equal = Self::sgn0(u) ^ Self::sgn0(y);
-    
-        let tv1: Fq = Fq::zero() - y;
-        if signs_not_equal == 0 {y = y} else {y = tv1}
 
+        #[allow(non_snake_case)]
+        let signsNotEqual = Self::sgn0(u) ^ Self::sgn0(y);
+
+        let tv1: Fq = Fq::zero() - y;
+
+        if signsNotEqual == 0 {y = y} else {y = tv1}
+        
         AffineG1::new(x, y)
     }
 
@@ -162,5 +170,125 @@ impl HashToG1 for AffineG1 {
         let q_1 = Self::map_to_curve(u[1]).unwrap();
         let q = q_0 + q_1;
         q
+    }
+}
+
+fn print_fq_as_bytes(fq: Fq) {
+    let bytes = unsafe { transmute::<_, [u8; 32]>(fq) };
+    // println!("bytes: {:?}", bytes);
+}
+
+#[cfg(test)]
+mod tests {
+    use std::mem::transmute;
+
+    use super::*;
+
+
+    #[test]
+    fn test_map_to_curve() {
+        // let u = Fq::hash_to_field(b"abc", b"QUUX-V01-CS02-with-BN254G1_XMD:SHA-256_SVDW_RO_", 2);
+        // assert!(u[0] == Fq::from_str("7951370986911800256774597109927097176311261202951929331835478768207980370345").unwrap());
+        // assert!(u[1] == Fq::from_str("8293556689416303717881563281438712057465092967957999993252567763605862533321").unwrap());
+
+        // let u0_bytes = unsafe { transmute::<_, [u8; 32]>(u[0]) };
+        // let u1_bytes = unsafe { transmute::<_, [u8; 32]>(u[1]) };
+        // println!("u0_bytes: {:?}", u0_bytes);
+        // println!("u1_bytes: {:?}", u1_bytes);
+
+        // let q0 = AffineG1::map_to_curve(u[0]).unwrap();
+        // let q1 = AffineG1::map_to_curve(u[1]).unwrap();
+        // println!("q0: {:?}", q0);
+        // println!("q1: {:?}", q1);
+        // // assert!(q0 == AffineG1::new(Fq::from_str("9192524283969255398734814822241735402343760142215332184598869386265143635853").unwrap(), Fq::from_str("14750013374492649779039522357455217122947104756064249167130349093550158884161").unwrap()).unwrap());
+        // // assert!(q1 == AffineG1::new(Fq::from_str("2219529064992744478098731193326567804904209297389738932911685687632211367327").unwrap(), Fq::from_str("1910726159786414357764375718946103460897900837832114831609513656424867805207").unwrap()).unwrap());
+        // let q0_expected = AffineG1::new(Fq::from_str("9192524283969255398734814822241735402343760142215332184598869386265143635853").unwrap(), Fq::from_str("14750013374492649779039522357455217122947104756064249167130349093550158884161").unwrap()).unwrap();
+        // let q1_expected = AffineG1::new(Fq::from_str("2219529064992744478098731193326567804904209297389738932911685687632211367327").unwrap(), Fq::from_str("1910726159786414357764375718946103460897900837832114831609513656424867805207").unwrap()).unwrap();
+
+        // let q0_bytes = unsafe { transmute::<_, [u8; 64]>(q0) };
+        // let q1_bytes = unsafe { transmute::<_, [u8; 64]>(q1) };
+        // let q0_expected_bytes = unsafe { transmute::<_, [u8; 64]>(q0_expected) };
+        // let q1_expected_bytes = unsafe { transmute::<_, [u8; 64]>(q1_expected) };
+
+        // println!("q0_expected_bytes: {:?}", q0_expected_bytes);
+        // println!("q1_expected_bytes: {:?}", q1_expected_bytes);
+
+
+        let u = Fq::hash_to_field(b"abcdef0123456789", b"QUUX-V01-CS02-with-BN254G1_XMD:SHA-256_SVDW_RO_", 2);
+        assert!(u[0] == Fq::from_str("21473511429296129787161665655193361189518945362859158450118183976151186446397").unwrap());
+        assert!(u[1] == Fq::from_str("17399580852346357386985693124899680967448413221719274165687915620563859110222").unwrap());
+        let q0 = AffineG1::map_to_curve(u[0]).unwrap();
+        let q1 = AffineG1::map_to_curve(u[1]).unwrap();
+        assert!(q0 == AffineG1::new(Fq::from_str("18460180777384996805517037410124907200489198402642233028065858702876325100173").unwrap(), Fq::from_str("7297925201307108404837100086863759533322513325723985709501528779399363778017").unwrap()).unwrap());
+        assert!(q1 == AffineG1::new(Fq::from_str("3555154583542724794659651262588560064541528505277497563560719769602741821875").unwrap(), Fq::from_str("16977637197741440727690443467244845071598833410411827382713029829487302630942").unwrap()).unwrap());
+
+        // let u = Fq::hash_to_field(b"", b"QUUX-V01-CS02-with-BN254G1_XMD:SHA-256_SVDW_RO_", 2);
+        // assert!(u[0] == Fq::from_str("21498498956904532351723378912032873852253513037650692457560050969314502748597").unwrap());
+        // assert!(u[1] == Fq::from_str("3106428082009635406807032300288584059640244342225966151234406580587112112014").unwrap());
+        // let q0 = AffineG1::map_to_curve(u[0]).unwrap();
+        // let q1 = AffineG1::map_to_curve(u[1]).unwrap();
+        // assert!(q0 == AffineG1::new(Fq::from_str("6453599284581821454252898427469570073430843606970728650145294868078481709202").unwrap(), Fq::from_str("18995581315822946008285423533984677217009732542182181378734620089887646003813").unwrap()).unwrap());
+        // assert!(q1 == AffineG1::new(Fq::from_str("11407741707599100220112369632304941265828026024296299145123573579681208493329").unwrap(), Fq::from_str("10936143794657572576642578819087135925019845836839797797601194413922673415908").unwrap()).unwrap());
+
+        // let u = Fq::hash_to_field(b"a512_aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", b"QUUX-V01-CS02-with-BN254G1_XMD:SHA-256_SVDW_RO_", 2);
+        // assert!(u[0] == Fq::from_str("2044513137826275527915612741016000753813717898656440700304636055936191489587").unwrap());
+        // assert!(u[1] == Fq::from_str("11602613730878338430727365363851039884306398846852682736694594518413917134846").unwrap());
+        // let q0 = AffineG1::map_to_curve(u[0]).unwrap();
+        // let q1 = AffineG1::map_to_curve(u[1]).unwrap();
+        // assert!(q0 == AffineG1::new(Fq::from_str("15648482829240231450830106706414350609765304380572340182587624553168656871227").unwrap(), Fq::from_str("2884090034870953736753092279678539049499634063513478774348194493913603274393").unwrap()).unwrap());
+        // assert!(q1 == AffineG1::new(Fq::from_str("18124086957708989159022363715051003299508967919164499453496316230748020813590").unwrap(), Fq::from_str("16826684847259413750643448296038340935081016395629901184485922600208379683908").unwrap()).unwrap());
+
+        // // msg: "q128_qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq", P: point{"0xfe2b0743575324fc452d590d217390ad48e5a16cf051bee5c40a2eba233f5c", "0x794211e0cc72d3cbbdf8e4e5cd6e7d7e78d101ff94862caae8acbe63e9fdc78"},
+        // // Q0: point{"0x1c53b05f2fce15ba0b9100650c0fb46de1fb62f1d0968b69151151bd25dfefa4", "0x1fe783faf4bdbd79b717784dc59619106e4acccfe3b5d9750799729d855e7b81"},
+        // // Q1: point{"0x214a4e6e97adda47558f80088460eabd71ed35bc8ceafb99a493dd6f4e2b3f0a", "0xfaaeb29cc23f9d09b187a99741613aed84443e7c35736258f57982d336d13bd"},
+        // // u0: "0x2a50be15282ee276b76db1dab761f75401cdc8bd9fff81fcf4d428db16092a7b", u1: "0x23b41953676183c30aca54b5c8bd3ffe3535a6238c39f6b15487a5467d5d20eb",
+
+        // let u = Fq::hash_to_field(b"q128_qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq", b"QUUX-V01-CS02-with-BN254G1_XMD:SHA-256_SVDW_RO_", 2);
+        // assert!(u[0] == Fq::from_str("19139799307876008157674469077244497844490197231122854489816996874209678928507").unwrap());
+        // assert!(u[1] == Fq::from_str("16149156964295957170548772524136742336424608142546544142472739268994996707563").unwrap());
+        // let q0 = AffineG1::map_to_curve(u[0]).unwrap();
+        // let q1 = AffineG1::map_to_curve(u[1]).unwrap();
+        // assert!(q0 == AffineG1::new(Fq::from_str("12812625340294489398993548898688649895606244530534785322892890577243153100708").unwrap(), Fq::from_str("14430750872577414903993343696062677117017041221676892932403418483169263778689").unwrap()).unwrap());
+        // assert!(q1 == AffineG1::new(Fq::from_str("15057612003824249181576746168110806738223995458659553230425471086211724164874").unwrap(), Fq::from_str("7086679767009137399570643369757025464320023320148085000688641996630730281917").unwrap()).unwrap());
+    }
+
+
+    #[test]
+    fn test_hash2field() {
+        // dst: []byte("QUUX-V01-CS02-with-BN254G1_XMD:SHA-256_SVDW_RO_"),
+        // msg: "abc", P: point{"0x23f717bee89b1003957139f193e6be7da1df5f1374b26a4643b0378b5baf53d1", "0x4142f826b71ee574452dbc47e05bc3e1a647478403a7ba38b7b93948f4e151d"},
+        // Q0: point{"0x1452c8cc24f8dedc25b24d89b87b64e25488191cecc78464fea84077dd156f8d", "0x209c3633505ba956f5ce4d974a868db972b8f1b69d63c218d360996bcec1ad41"},
+        // Q1: point{"0x4e8357c98524e6208ae2b771e370f0c449e839003988c2e4ce1eaf8d632559f", "0x4396ec43dd8ec8f2b4a705090b5892219759da30154c39490fc4d59d51bb817"},
+        // u0: "0x11945105b5e3d3b9392b5a2318409cbc28b7246aa47fa30da5739907737799a9", u1: "0x1255fc9ad5a6e0fb440916f091229bda611c41be2f2283c3d8f98c596be4c8c9",
+        let u = Fq::hash_to_field(b"abc", b"QUUX-V01-CS02-with-BN254G1_XMD:SHA-256_SVDW_RO_", 2);
+        assert!(u[0] == Fq::from_str("7951370986911800256774597109927097176311261202951929331835478768207980370345").unwrap());
+        assert!(u[1] == Fq::from_str("8293556689416303717881563281438712057465092967957999993252567763605862533321").unwrap());
+
+        // msg: "abcdef0123456789", P: point{"0x187dbf1c3c89aceceef254d6548d7163fdfa43084145f92c4c91c85c21442d4a", "0xabd99d5b0000910b56058f9cc3b0ab0a22d47cf27615f588924fac1e5c63b4d"},
+        // Q0: point{"0x28d01790d2a1cc4832296774438acd46c2ce162d03099926478cf52319daba8d", "0x10227ab2707fd65fb45e87f0a48cfe3556f04113d27b1da9a7ae1709007355e1"},
+        // Q1: point{"0x7dc256c7aadac1b4e1d23b3b2bbb5e2ffd9c753b9073d8d952ead8f812ce1b3", "0x2589008b2e15dcb3d16cdc1fed2634778001b1b28f0ab433f4f5ec6635c55e1e"},
+        // u0: "0x2f7993a6b43a8dbb37060e790011a888157f456b895b925c3568690685f4983d", u1: "0x2677d0532b47a4cead2488845e7df7ebc16c0b8a2cd8a6b7f4ce99f51659794e",
+
+        let u = Fq::hash_to_field(b"abcdef0123456789", b"QUUX-V01-CS02-with-BN254G1_XMD:SHA-256_SVDW_RO_", 2);
+        assert!(u[0] == Fq::from_str("21473511429296129787161665655193361189518945362859158450118183976151186446397").unwrap());
+        assert!(u[1] == Fq::from_str("17399580852346357386985693124899680967448413221719274165687915620563859110222").unwrap());
+
+        // msg: "", P: point{"0xa976ab906170db1f9638d376514dbf8c42aef256a54bbd48521f20749e59e86", "0x2925ead66b9e68bfc309b014398640ab55f6619ab59bc1fab2210ad4c4d53d5"},
+        // Q0: point{"0xe449b959abbd0e5ab4c873eaeb1ccd887f1d9ad6cd671fd72cb8d77fb651892", "0x29ff1e36867c60374695ee0c298fcbef2af16f8f97ed356fa75e61a797ebb265"},
+        // Q1: point{"0x19388d9112a306fba595c3a8c63daa8f04205ad9581f7cf105c63c442d7c6511", "0x182da356478aa7776d1de8377a18b41e933036d0b71ab03f17114e4e673ad6e4"},
+        // u0: "0x2f87b81d9d6ef05ad4d249737498cc27e1bd485dca804487844feb3c67c1a9b5", u1: "0x6de2d0d7c0d9c7a5a6c0b74675e7543f5b98186b5dbf831067449000b2b1f8e",
+
+        let u = Fq::hash_to_field(b"", b"QUUX-V01-CS02-with-BN254G1_XMD:SHA-256_SVDW_RO_", 2);
+        assert!(u[0] == Fq::from_str("21498498956904532351723378912032873852253513037650692457560050969314502748597").unwrap());
+        assert!(u[1] == Fq::from_str("3106428082009635406807032300288584059640244342225966151234406580587112112014").unwrap());
+
+        // msg: "a512_aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", P: point{"0x1b05dc540bd79fd0fea4fbb07de08e94fc2e7bd171fe025c479dc212a2173ce", "0x1bf028afc00c0f843d113758968f580640541728cfc6d32ced9779aa613cd9b0"},
+        // Q0: point{"0x2298ba379768da62495af6bb390ffca9156fde1dc167235b89c6dd008d2f2f3b", "0x660564cf6fce5cdea4780f5976dd0932559336fd072b4ddd83ec37f00fc7699"},
+        // Q1: point{"0x2811dea430f7a1f6c8c941ecdf0e1e725b8ad1801ad15e832654bd8f10b62f16", "0x253390ed4fb39e58c30ca43892ab0428684cfb30b9df05fc239ab532eaa02444"},
+        // u0: "0x48527470f534978bae262c0f3ba8380d7f560916af58af9ad7dcb6a4238e633", u1: "0x19a6d8be25702820b9b11eada2d42f425343889637a01ecd7672fbcf590d9ffe",
+
+        let u = Fq::hash_to_field(b"a512_aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", b"QUUX-V01-CS02-with-BN254G1_XMD:SHA-256_SVDW_RO_", 2);
+        assert!(u[0] == Fq::from_str("2044513137826275527915612741016000753813717898656440700304636055936191489587").unwrap());
+        assert!(u[1] == Fq::from_str("11602613730878338430727365363851039884306398846852682736694594518413917134846").unwrap());
     }
 }
